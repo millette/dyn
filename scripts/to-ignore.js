4545110e35ab08302cd03cbc19ffa00d7ce189de
Robin Millette <robin@millette.info>
2017-06-11 18:09:47 -0400

    Touchups.

--- scripts/to-ignore.js
+++ scripts/to-ignore.js
@@ -10,13 +10,10 @@ const glob = pify(require('glob'))
 const readFile = pify(fs.readFile)
 const writeFileImp = pify(fs.writeFile)
 
-const writeFile = (x) => writeFileImp('.datignore', `${x.join('\n')}\n`, 'utf-8')
-
-const ignoreWhat = (keep) => glob('bower_components/**', {
-  ignore: keep,
-  dot: true,
-  nodir: true
-})
+const writeFile = (fn, x) => {
+  const cnt = `${x.join('\n')}\n`
+  return Promise.all([cnt.length, writeFileImp(fn, cnt, 'utf-8')])
+}
 
 const inUse = () => readFile('index.html', 'utf-8')
   .then((x) => {
@@ -28,15 +25,18 @@ const inUse = () => readFile('index.html', 'utf-8')
     return ok.sort()
   })
 
+// FIXME: instead of nodir=true, only exclude unused paths
+const ignoreWhat = (ignore) => glob('bower_components/**', { ignore, dot: true, nodir: true })
 const keepGit = ['', 'bower_components', '.datignore']
-
 const isGit = (x) => keepGit.indexOf(x) === -1
 
 const gitIgnored = (i) => readFile('.gitignore', 'utf-8')
   .then((x) => x.split('\n').filter(isGit).concat(i))
 
-inUse()
+const run = (fn) => inUse()
   .then(ignoreWhat)
   .then(gitIgnored)
-  .then(writeFile)
-  .catch(console.error)
+  .then(writeFile.bind(null, fn))
+  .then((x) => console.log(`Wrote ${fn} (${x[0]} bytes).`))
+
+run('.datignore').catch(console.error)

dc748fdef72902a08a3346402b959c3b6bbccd34
Robin Millette <robin@millette.info>
2017-06-10 23:00:06 -0400

    Use dat-changes component.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
   const selfArchive = new DatArchive(String(w.location))
   const archiveInfo = await selfArchive.getInfo()
   archiveInfo.networkEvents = selfArchive.createNetworkActivityStream()
+  archiveInfo.fileEvents = selfArchive.createFileActivityStream()
+
   riot.mount('page-title', archiveInfo)
   riot.mount('dat-meta', archiveInfo)
+  riot.mount('dat-changes', archiveInfo)
 })(window)
